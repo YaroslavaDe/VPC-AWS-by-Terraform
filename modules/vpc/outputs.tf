a181ed3630025fa86fabab23d39fb80fc312f3d1
Yara Derk <yara.vikt@gmail.com>
2022-10-09 15:26:38 +0300

    alb

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -13,3 +13,11 @@ output "vpc_id" {
 output "internet_gateway" {
   value = aws_internet_gateway.this
 }
+
+output "public_subnet_id" {
+  value = aws_subnet.public[*].id
+
+}
+output "private_subnet_id" {
+  value = aws_subnet.private[*].id
+}
