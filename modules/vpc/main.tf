eb929a1b59bf46afd08488013de33ccfd191a96d
Yara Derk <yara.vikt@gmail.com>
2022-10-09 11:37:52 +0300

    dynamic

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "vpc" {
 }
 
 # create internet gateway and attach it to vpc
-resource "aws_internet_gateway" "internet_gateway" {
+resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.vpc.id
 
   tags = {
@@ -23,8 +23,9 @@ data "aws_availability_zones" "available" {
 }
 
 # public subnets configuration
-resource "aws_subnet" "public_subnet" {
-  for_each                = toset(var.public_subnet_CIDR)
+resource "aws_subnet" "public" {
+  for_each = toset(var.public_subnet_CIDR)
+
   vpc_id                  = aws_vpc.vpc.id
   availability_zone       = element(data.aws_availability_zones.available.names, index(var.public_subnet_CIDR, each.key))
   cidr_block              = each.key
@@ -34,12 +35,12 @@ resource "aws_subnet" "public_subnet" {
 }
 
 # create route table and add public route
-resource "aws_route_table" "public_route_table" {
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.vpc.id
 
   route {
     cidr_block = var.cidr_public_route
-    gateway_id = aws_internet_gateway.internet_gateway.id
+    gateway_id = aws_internet_gateway.this.id
   }
 
   tags = {
@@ -48,15 +49,17 @@ resource "aws_route_table" "public_route_table" {
 }
 
 # associate public subnets to "public route table"
-resource "aws_route_table_association" "public_subnet_route_table_association" {
-  for_each       = toset(var.public_subnet_CIDR)
-  subnet_id      = aws_subnet.public_subnet[each.value].id
-  route_table_id = aws_route_table.public_route_table.id
+resource "aws_route_table_association" "public" {
+  for_each = toset(var.public_subnet_CIDR)
+
+  subnet_id      = aws_subnet.public[each.value].id
+  route_table_id = aws_route_table.public.id
 }
 
 # create private subnets
-resource "aws_subnet" "private_subnet" {
-  for_each                = toset(var.private_subnet_CIDR)
+resource "aws_subnet" "private" {
+  for_each = toset(var.private_subnet_CIDR)
+
   vpc_id                  = aws_vpc.vpc.id
   availability_zone       = element(data.aws_availability_zones.available.names, index(var.private_subnet_CIDR, each.key))
   cidr_block              = each.key
@@ -64,6 +67,43 @@ resource "aws_subnet" "private_subnet" {
 
   tags = { Name = join("-", ["private", element(reverse(split("-", each.value)), 0)]) }
 }
+
+# allocate elastic ip
+# eip will be used for the nat-gateway in the public subnets
+resource "aws_eip" "nat" {
+  vpc  = true
+  tags = { Name = "nat" }
+}
+
+# create nat gateway in public subnets
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[element(var.public_subnet_CIDR, 1)].id
+
+  tags = { Name = "ngw" }
+}
+
+# create private route table and add route through nat gateway 
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
+  }
+
+  tags = { Name = "private" }
+}
+
+# associate private subnets with private route table
+resource "aws_route_table_association" "private" {
+  for_each       = toset(var.private_subnet_CIDR)
+
+  route_table_id = aws_route_table.private.id
+  subnet_id      = aws_subnet.private[each.value].id
+}
+
+
 
 
 
